acd49ea35378c3a2530308c6c7671ee4a5e63b6a
diegomebro <diegomatias.mb@hotmail.com>
2016-07-18 21:24:21 -0400

    run migration

--- db/migrate/20160719004543_create_works.rb
+++ db/migrate/20160719004543_create_works.rb
@@ -1,7 +1,7 @@
 class CreateWorks < ActiveRecord::Migration
   def change
     create_table :works do |t|
-      t.txt :descriptionwork
+      t.text :descriptionwork
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718235510) do
+ActiveRecord::Schema.define(version: 20160719004543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,33 @@ ActiveRecord::Schema.define(version: 20160718235510) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "inventories", force: :cascade do |t|
+    t.integer  "serialnumber"
+    t.integer  "wheel"
+    t.text     "description"
+    t.integer  "category_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "inventories", ["category_id"], name: "index_inventories_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.text     "descriptionwork"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "works", ["user_id"], name: "index_works_on_user_id", using: :btree
+
+  add_foreign_key "inventories", "categories"
+  add_foreign_key "works", "users"
 end
